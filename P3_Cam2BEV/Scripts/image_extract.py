aa0f4bbe991f75697cd2e33193e8b701a1fe26f7
shivensingh2013 <shivendra.singh@in.bosch.com>
2023-11-28 16:58:21 +0530

    running script

--- P3_Cam2BEV/Scripts/image_extract.py
+++ P3_Cam2BEV/Scripts/image_extract.py
@@ -5,6 +5,8 @@ from nuscenes.nuscenes import NuScenes
 import os 
 import shutil
 import yaml
+from Scripts.utils import quart_to_angles
+from Scripts.ipm import generate_ipm_image
 
 class cam2bev_image:
     
@@ -37,6 +39,7 @@ class cam2bev_image:
 
 
 class nuscene_image:
+
     def __init__(self,scene,dataroot_nuscene,dir,parent,out_parent):
         super().__init__()
         self.image_angles =  ["CAM_FRONT","CAM_BACK","CAM_FRONT_LEFT","CAM_FRONT_RIGHT","CAM_BACK_RIGHT","CAM_BACK_LEFT"]
@@ -48,82 +51,104 @@ class nuscene_image:
         self.scene_obj = self.nuscene_data_obj.scene[self.scene]
         self.first_sample_token = self.scene_obj['first_sample_token']
         self.out_parent = out_parent
+
+        rotation_dict = {}
+        rotation_dict["CAM_FRONT"]={"yaw":0,"pitch":0,"roll":0}
+        rotation_dict["CAM_BACK"]={"yaw":180,"pitch":0,"roll":0}
+        rotation_dict["CAM_FRONT_LEFT"]={"yaw":60,"pitch":0,"roll":0}
+        rotation_dict["CAM_FRONT_RIGHT"]={"yaw":-60,"pitch":0,"roll":0}
+        rotation_dict["CAM_BACK_RIGHT"]={"yaw":-120,"pitch":0,"roll":0}
+        rotation_dict["CAM_BACK_LEFT"]={"yaw":120,"pitch":0,"roll":0}
+
+        self.rotation_dict = rotation_dict
         return 
 
-    def create_folder(self):
-        path = self.parent+self.dir
+    def create_folder(self,n):
+        path = self.parent+self.dir+"\\"+str(n)
+
+        if not os.path.exists(self.parent+self.dir):
+             os.mkdir(self.parent+self.dir)
+
         if not os.path.exists(path):
             os.mkdir(path)
             os.mkdir(path+"\images")
             os.mkdir(path+"\config_logs")
             os.mkdir(path+"\config")
-            print("Created folders")
         else:
             print("path already exists ,  Change the output folder name")
         return 
 
-    # def get_sample(self,sample_token):
-    #     self.sample_token = sample_token
-    #     my_sample = self.nuscene_data_obj.get('sample', sample_token)
-    #     return my_sample
+    def generate_1_bev(self,sample,n):
+        ##Generate folder structure for input
+        print("processing sample",sample)
+        self.create_folder(n)
 
-
-
-    def generate_1_bev(self,sample):
+        ## create input config and image files for IPM ops
+        camera_img_pair = []
         for angle in self.image_angles:
             json_info =  self.nuscene_data_obj.get('sample_data', sample['data'][angle])
             file_name =json_info["filename"]
-            ego_pose =self.nuscene_data_ob.get("ego_pose", json_info["ego_pose_token"])
-            calibrated_sensor_data = self.nuscene_data_ob.get("calibrated_sensor",json_info["calibrated_sensor_token"])
+            ego_pose =self.nuscene_data_obj.get("ego_pose", json_info["ego_pose_token"])
+            calibrated_sensor_data = self.nuscene_data_obj.get("calibrated_sensor",json_info["calibrated_sensor_token"])
             # store the image in the input file path
-            shutil.copyfile( self.dataroot_nuscene+file_name, self.parent+self.dir+"\images"+'\\'+angle+".jpg")
-
-            
-        ## Create config logs 
-            config_file_logs = self.parent+self.dir+"\config_logs\\"+angle+'.yaml'
-            
+            image_path  = self.parent+self.dir+"\\"+str(n)+"\images"+'\\'+angle+".jpg"
+            shutil.copyfile( self.dataroot_nuscene+file_name,image_path )
+            ## Create config logs 
+            config_file_logs = self.parent+self.dir+"\\"+str(n)+"\config_logs\\"+angle+'.yaml'
             with open(config_file_logs,'w') as cf:
                 yaml.dump({"sensor data":calibrated_sensor_data}  , cf, default_flow_style=False)
                 yaml.dump({"egopose":ego_pose}  , cf, default_flow_style=False)
             cf.close()
 
-        ## Create configs file from logs 
-            config_file = self.parent+self.dir+"\config\\"+angle+'.yaml'
-            cam2bev_config = create_cam2bev_config(self.parent+self.dir,calibrated_sensor_data,ego_pose )
-
+            ## Create configs file from logs 
+            config_file = self.parent+self.dir+"\\"+str(n)+"\config\\"+angle+'.yaml'
+            cam2bev_config = self.create_cam2bev_config(self.parent+self.dir,calibrated_sensor_data,ego_pose,angle)
             with open(config_file,'w') as cf:
                 yaml.dump(  cam2bev_config, cf, default_flow_style=False)
+                cf.close()
+            camera_img_pair.append(config_file)
+            camera_img_pair.append(image_path)
 
-            # yaml.dump( , cf, default_flow_style=False)
-        ## Create 1 drone yaml file from logs 
 
-        # drone = path+"\config\drone.yaml'
-        # with open(drone,'w') as cf:
+        ## Drone files to be copied as is from master
+        drone_file_loc = r"C:\Users\IHG6KOR\Desktop\shiv\Portfolio\shivensingh2013.github.io\P3_Cam2BEV\Scripts\drone.yaml"
+        shutil.copyfile( drone_file_loc, self.parent+self.dir+"\\"+str(n)+"\config\drone.yaml")
+        # create_output_folder()
+        # Run IPM
+        args = {}
+        args["output"]= self.create_output_folder()
+        args["batch"] = False
+        args["camera_img_pair"] = camera_img_pair
+        args["drone"] = self.parent+self.dir+"\\"+str(n)+"\config\drone.yaml"
+        args["r"] = 20
+        args["wm"] = 20
+        args["hm"] =40
+        args["v"] = False
+        args["cc"] = False
+        args["outfile_name"] = str(n) + ".jpg"
 
-            # yaml.dump( , cf, default_flow_style=False)
-        # cf.close()
-            # print(file_name)
-        print(len(self.image_angles) , "IMAGES LOADED TO INPUT FOLDER")
-        create_output_folder()
+        generate_ipm_image(args)
 
-        ## TODO - Need to run the ipm code with the right parameters
-        run_ipm()
         return
 
     def generate_n_BEV(self):
         next_token = self.scene_obj['first_sample_token']
-        while():
-            if next_token  == self.scene_obj['last_sample_token']:
+        count= 0
+        while(1>0):
+            count+=1
+            if next_token  == self.scene_obj['last_sample_token'] or count > 10:
                 break
             else: 
                 my_sample =  self.nuscene_data_obj.get('sample',next_token)   
-                generate_1_bev(my_sample)
+                self.generate_1_bev(my_sample,count)
                 next_token = my_sample["next"]
-        print("Generation of results complete")
+                # print("Now processing", next_token)
+            print(str(count) +" Generation complete")
         return 
 
-    def create_cam2bev_config(self, path, sensor, egopose):
-        result ={}
+    def create_cam2bev_config(self, path, sensor, egopose,angle):
+
+        result = {}
 
         result['fx'] = sensor["camera_intrinsic"][0][0]
         result['fy'] = sensor["camera_intrinsic"][1][1]
@@ -135,7 +160,8 @@ class nuscene_image:
         # result["yaw"],result["pitch"],result["roll"] =   quart_to_angles(sensor['rotation'])
 
         ## TODO - make yaw,pithc and roll as standard - rule based
-
+        res = self.rotation_dict[angle]
+        result["yaw"],result["pitch"],result["roll"] = res["yaw"],res["pitch"],res["roll"]
 
         ## translation
         result["XCam"] =sensor["translation"][0]
@@ -152,7 +178,16 @@ class nuscene_image:
         else:
             print("path already exists ,  check name of path")
 
+        return outpath
+
+
+
+    def run_ipm(self):
+
+
         return
+
+
 
     
 
--- P3_Cam2BEV/Scripts/utils.py
+++ P3_Cam2BEV/Scripts/utils.py
@@ -24,8 +24,8 @@ def quart_to_angles(rotation) -> tuple:
     pitch = math.asin(2 * ((q1 * q3) - (q0 * q2)))
     yaw = math.atan2( 2 * ((q1 * q2) + (q0 * q3)),q0**2 + q1**2 - q2**2 - q3**2)
 
-    # roll = roll* (180 / math.pi)
-    # pitch= pitch* (180 / math.pi)
-    # yaw = yaw* (180 / math.pi)
+    roll = roll* (180 / math.pi)
+    pitch= pitch* (180 / math.pi)
+    yaw = yaw* (180 / math.pi)
     return (yaw, pitch, roll)
 
